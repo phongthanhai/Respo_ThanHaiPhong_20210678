f35103f4b791f383140f995396ffb27a45e610e5
phongthanhai <126260318+phongthanhai@users.noreply.github.com>
2023-06-21 19:20:14 +0700

    Add files via upload

---
+++ Lab 5/AimsProject/src/vn/edu/hust/soict/test/storetest/StoreTest.java
@@ -0,0 +1,11 @@
+package vn.edu.hust.soict.test.storetest;
+
+import vn.edu.hust.soict.model.media.DigitalVideoDisc;
+import vn.edu.hust.soict.model.store.Store;
+
+public class StoreTest {
+	public static void main(String[] args) {
+        Store store = new Store();
+
+    }
+}
